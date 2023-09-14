f838e27635c491d9467124be649b295f2ca7a804
Sirajul Islam <ayasabegum134@gmail.com>
2023-09-14 22:08:22 +0600

    i create Component and send data show Course Box

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import CourseCards from './components/CoursePlayListCards/CoursesCard/CourseCard
 function App() {
 
   return (
-    <>
+    <div className='max-w-screen-xl mx-auto'>
       
-      <h1>Course Registration</h1>
+      <h1 className='text-center text-4xl mt-12 mb-8 font-bold'>Course Registration</h1>
       <div>
         <CourseCards></CourseCards>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/CourseDetailsBox/CourseBoxs.jsx
+++ src/components/CourseDetailsBox/CourseBoxs.jsx
@@ -1,8 +1,17 @@
 
 const CourseBoxs = () => {
     return (
-        <div>
-            
+        <div className="bg-white p-6 rounded-lg">
+            <h2 className="text-[#2F80ED] text-lg font-bold">Credit Hour Remaining <span></span>hr</h2>
+            <hr className="my-4" />
+            <h1 className="text-[#1C1B1B] mb-5 font-bold text-xl">Course Name</h1>
+            <div className="text-[#1C1B1B99]">
+
+            </div>
+            <hr className="mt-6 mb-4"/>
+            <h3 className="text-lg text-[#1C1B1BCC] font-medium">Total Credit Hour : <span></span></h3>
+            <hr className="my-4"/>
+            <h3 className="text-lg text-[#1C1B1BCC] font-bold">Total Price : <span></span> USD</h3>
         </div>
     );
 };
